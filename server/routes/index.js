821bd560f595339dc16a1521a03ef3539558f91a
Zachary Lester <zachary.greg.lester@gmail.com>
2016-07-28 21:52:06 -0700

    Mount championRouter

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,7 @@
 const leagueRouter = require('../resources/league/leagueRouter');
+const championRouter = require('../resources/champions/championRouter');
 
 module.exports = app => {
   app.use('/api/league', leagueRouter);
+  app.use('/api/champions', championRouter);
 };
