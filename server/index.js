ddf9b4f26430f09fb8ca1d795d910d20d4052be4
Zachary Lester <zachary.greg.lester@gmail.com>
2016-07-30 17:43:28 -0700

    Add node cluster module

--- server/index.js
+++ server/index.js
@@ -1,6 +1,25 @@
-const app = require('./server');
-const { PORT } = require('./config');
+const logger = require('winston');
+const cluster = require('cluster');
 
-app.listen(PORT, () => {
-  console.log(`Blitzcranky listening on port ${PORT}`);
-});
+if (cluster.isMaster) {
+  const cpus = require('os').cpus();
+  cpus.forEach(cpu => {
+    cluster.fork();
+  });
+
+  cluster.on('online', worker => {
+    logger.info(`Worker ${worker.id} started.`);
+  });
+
+  cluster.on('exit', (worker, code) => {
+    logger.warn(`Worker ${worker.id} died with code ${code}. Restarting new worker.`);
+    cluster.fork();
+  });
+} else {
+  const app = require('./server');
+  const { PORT } = require('./config');
+
+  app.listen(PORT, () => {
+    logger.info(`Blitzcranky Static Server Worker ${cluster.worker.id} listening on port ${PORT}`);
+  });
+}
