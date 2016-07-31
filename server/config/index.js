a6d9f29de27386e274980d479726185a3419c43d
Zachary Lester <zachary.greg.lester@gmail.com>
2016-07-30 23:57:29 -0700

    Modify CPU calculation

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
   REDIS_URL: process.env.REDIS_URL || null,
-  MAX_WORKERS: process.env.WEB_CONCURRENCY || require('os').cpus.length,
+  MAX_WORKERS: process.env.WEB_CONCURRENCY || require('os').cpus().length,
 };
