4c409e8d6a123bf567e7ddecb29af67002b040c2
Zachary Lester <zachary.greg.lester@gmail.com>
2016-07-31 19:50:32 -0700

    Add server tests

---
+++ test/server-spec.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+const app = require('../server/server.js');
+
+chai.use(chaiHttp);
+
+describe('Server Functionality', () => {
+
+  it('should exist', () => {
+    expect(app).to.exist;
+  });
+
+  it('should run in development when testing', () => {
+    expect(app.settings.env).to.equal('development');
+  });
+
+  it('should serve static files on a GET to /', done => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(err).to.equal(null);
+        expect(res.status).to.equal(200);
+        expect(res.ok).to.equal(true);
+        expect(res.headers['content-type']).to.equal('text/html; charset=UTF-8');
+        done();
+      });
+  });
+});
